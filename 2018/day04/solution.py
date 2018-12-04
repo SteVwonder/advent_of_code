478c684c0c313520f0308c053f9bdabb4218e9c8
Stephen Herbein <stephen272@gmail.com>
2018-12-03 22:28:52 -0800

    refactoring of solution for day04

--- 2018/day04/solution.py
+++ 2018/day04/solution.py
@@ -2,78 +2,90 @@
 
 import re
 import sys
+import functools
 from datetime import datetime
 from collections import namedtuple, defaultdict
 
 import numpy as np
 
+def star(f):
+    @functools.wraps(f)
+    def f_inner(args):
+        return f(*args)
+    return f_inner
+
 parsing_re = re.compile(r'\[([0-9-]+ [0-9:]+)] (.*)')
-def parse_line(line):
+def parse_time_from_line(line):
     match_obj = parsing_re.match(line)
     timestamp, event_str = match_obj.group(1, 2)
     time = datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
     return (time, event_str)
 
-event_order = {'G': 0,
-               'f' : 1,
-               'w' : 2}
+event_order = {'Guard': 0,
+               'falls' : 1,
+               'wakes' : 2}
 def parsed_line_sort_key(parsed_line):
     time, event_str = parsed_line
-    return time, event_order[event_str[0]]
+    event_first_word = event_str.split(" ")[0]
+    return time, event_order[event_first_word]
 
+guard_re = re.compile(r'Guard #(\d+) begins shift')
+def strided_access(lines):
+    lines = iter(lines)
+    guard_line = next(lines)
+    while True:
+        try:
+            next_line = next(lines)
+        except StopIteration:
+            return
+        # Check if the same guard fell asleep again
+        if guard_re.match(next_line[1]) is None:
+            # the same guard fell asleep again....slacker!
+            yield(guard_line, next_line, next(lines))
+        else:
+            # we are dealing with a new guard
+            # loop in case they didn't fall asleep
+            guard_line = next_line
+
+Event = namedtuple("Event", ['id', 'asleep_at', 'awake_at', 'asleep_for'])
 def main(filename):
     with open(filename, 'r') as infile:
         lines = [line.strip() for line in infile.readlines()]
+    parsed_lines = sorted([parse_time_from_line(line) for line in lines], key=parsed_line_sort_key)
 
-    Event = namedtuple("Event", ['id', 'asleep_at', 'awake_at', 'asleep_for'])
-
-    parsed_lines = sorted([parse_line(line) for line in lines], key=parsed_line_sort_key)
-
-    guard_re = re.compile(r'Guard #(\d+) begins shift')
-    curr_guard = -1
-    asleep_at = -1
-    awake_at = -1
     event_tuples = []
     guards = defaultdict(list)
-    for time, event_str in parsed_lines:
-        match_obj = guard_re.match(event_str)
-        if match_obj is not None:
-            curr_guard = int(match_obj.group(1))
-            asleep_at = -1
-            awake_at = -1
-        elif event_str == "falls asleep":
-            asleep_at = time
-        elif event_str == "wakes up":
-            awake_at = time
-            if asleep_at == -1:
-                print(f"Guard {curr_guard} doesn't have an asleep time")
-            asleep_for = ((awake_at - asleep_at).total_seconds() / 60)
-            event = Event(curr_guard, asleep_at, awake_at, asleep_for)
-            event_tuples.append(event)
-            guards[curr_guard].append(event)
+    for guard_line, asleep_line, wake_line in strided_access(parsed_lines):
+        match_obj = guard_re.match(guard_line[1])
+        assert match_obj is not None
+        curr_guard = int(match_obj.group(1))
 
-    total_time_asleep = defaultdict(lambda: 0)
-    for guard_id, event_list in guards.items():
-        for event in event_list:
-            total_time_asleep[guard_id] += event.asleep_for
-    sleepiest_guard = max(total_time_asleep.items(), key=lambda x: x[1])[0]
+        asleep_at = asleep_line[0]
+        assert asleep_line[1] == "falls asleep"
 
-    minutes_asleep_hist = np.zeros(60)
-    for event in guards[sleepiest_guard]:
-        minutes_asleep_hist[event.asleep_at.minute:event.awake_at.minute] += 1
-    sleepiest_minute = minutes_asleep_hist.argmax()
-    print(f'Part 1: {sleepiest_minute * sleepiest_guard}')
+        awake_at = wake_line[0]
+        assert wake_line[1] == "wakes up"
 
+        asleep_for = ((awake_at - asleep_at).total_seconds() / 60)
 
-    strat2_tuples = []
+        event = Event(curr_guard, asleep_at, awake_at, asleep_for)
+        event_tuples.append(event)
+        guards[curr_guard].append(event)
+
+    # build a histogram of sleepy minutes for each guard
+    sleeping_stats = {}
     for guard_id, event_list in guards.items():
         minutes_asleep_hist = np.zeros(60)
         for event in event_list:
             minutes_asleep_hist[event.asleep_at.minute:event.awake_at.minute] += 1
-        sleepiest_minute = minutes_asleep_hist.argmax()
-        strat2_tuples.append((minutes_asleep_hist[sleepiest_minute], sleepiest_minute, guard_id))
-    strat2_matching_tuple = max(strat2_tuples, key=lambda x: x[0])
-    print(f'Part 2: {strat2_matching_tuple[1]*strat2_matching_tuple[2]}')
+        sleeping_stats[guard_id] = minutes_asleep_hist
+
+    # sum gets us the guard that slept the most overall
+    # max gets us the guard with the sleepiest specific minute
+    for idx, func in enumerate([sum, max]):
+        sleepiest_guard, sleepiest_hist = max(sleeping_stats.items(), key=star(lambda guard, hist: func(hist)))
+        sleepiest_minute = sleepiest_hist.argmax()
+        print(f'Part {idx}: {sleepiest_minute * sleepiest_guard}')
 
 if len(sys.argv) > 1:
     main(sys.argv[1])
