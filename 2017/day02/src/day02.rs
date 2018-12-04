4e14ada92a35b56bab70b587a375e39e84a41365
Stephen Herbein <stephen272@gmail.com>
2018-12-03 20:57:12 -0800

    add stuff from 2017 :shrug:

--- 2017/day02/src/day02.rs
+++ 2017/day02/src/day02.rs
@@ -2,11 +2,13 @@ extern crate csv;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
+extern crate time;
 
 use std::path::Path;
 use log::LogLevel;
+use time::PreciseTime;
 
-fn process(path: &Path) -> u32 {
+fn part1(path: &Path) -> u32 {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .flexible(true)
@@ -29,6 +31,40 @@ fn process(path: &Path) -> u32 {
     max_diffs.iter().sum()
 }
 
+fn part1(path: &Path) -> u32 {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .flexible(true)
+        .delimiter(b'\t')
+        .from_path(path)
+        .unwrap();
+    let divisible_vals: Vec<u32> = rdr.records()
+        .map(|result| {
+            let record = result.unwrap();
+            debug!("{:?}", record);
+            let integers: Vec<u32> = record
+                .iter()
+                .map(|val| val.parse::<u32>().unwrap())
+                .collect();
+            find_divisble_vals
+            debug!("Max diff: {}", max_diff);
+            max_diff
+        })
+        .collect();
+    divisible_vals.iter().sum()
+}
+
+fn timed_run(f: fn(&Path) -> u32, arg: &Path) -> u32{
+    let start = PreciseTime::now();
+    let sum = f(arg);
+    let end = PreciseTime::now();
+    let microsec = start.to(end).num_microseconds().unwrap_or(0);
+    let millisec: f64 = (microsec as f64) / 1000.0;
+    println!("Exec took {} ms", millisec);
+
+    sum
+}
+
 fn main() {
     env_logger::init().unwrap();
 
@@ -37,6 +73,8 @@ fn main() {
         false => Path::new("data/input"),
     };
 
-    let sum = process(path);
+    let sum = timed_run(part1, path);
     println!("The answer to part 1 is: {}", sum);
+    let sum = timed_run(part2, path);
+    println!("The answer to part 2 is: {}", sum);
 }
