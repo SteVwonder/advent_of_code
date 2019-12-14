c7c9d78f13ae8e8ef45f0d67ac7de3f7e19ec457
Stephen Herbein <stephen272@gmail.com>
2019-12-14 12:31:41 -0800

    Refactor 2019 Day 01

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,55 +1,51 @@
-use std::path::Path;
-use std::io::{BufReader,BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
-fn fuel_calc_v2(score: f32, debug: bool) -> f32 {
-    let mut fuel_req = (score / 3.0).floor() - 2.0;
-    if fuel_req < 0.0 {
-        return 0.0;
+fn fuel_calc_v2(score: i32) -> i32 {
+    let fuel_req = fuel_calc_v1(score);
+    if fuel_req < 0 {
+        return 0;
     }
-    fuel_req += fuel_calc_v2(fuel_req, false);
-    if debug {
-        println!("{} requires {} fuel", score, fuel_req);
-    }
-    fuel_req
+    fuel_req + fuel_calc_v2(fuel_req)
 }
 
-fn fuel_calc_v1(score: f32, debug: bool) -> f32 {
-    let fuel_req = (score / 3.0).floor() - 2.0;
-    if debug {
-        println!("{} requires {} fuel", score, fuel_req);
-    }
-    fuel_req as f32
+fn fuel_calc_v1(score: i32) -> i32 {
+    (score as f32 / 3.0).floor() as i32 - 2
 }
 
-fn solve(path: &Path, fuel_calc: fn(f32, bool) -> f32, debug: bool) -> u32{
+fn solve(path: &Path, fuel_calc: fn(i32) -> i32, verbose: bool) -> i32 {
     let display = path.display();
     let file = match File::open(&path) {
-        Err(why) => panic!(
-            "couldn't open {}: {}", display, why),
+        Err(why) => panic!("couldn't open {}: {}", display, why),
         Ok(file) => file,
     };
 
     let reader = BufReader::new(file);
     let scores = reader.lines().map(|line| {
-        let score: f32 = line.unwrap().parse().unwrap();
-        score
+        line.unwrap().parse::<i32>().unwrap()
     });
     let fuel_reqs = scores.map(|score| {
-        fuel_calc(score, debug) as u32
+        let fuel_req = fuel_calc(score);
+        if verbose {
+            println!("{} requires {} fuel", score, fuel_req);
+        }
+        fuel_req
     });
-    let total : u32 = fuel_reqs.sum();
-    return total;
+    fuel_reqs.sum()
 }
 
 fn main() {
-    for (idx, func) in [fuel_calc_v1, fuel_calc_v2].iter().enumerate() {
-        println!("======Part {}=====", idx+1);
-        println!("==Test Case==");
-        let mut answer : u32 = solve(Path::new("./test"), *func, true);
-        println!("Total Fuel Requirements: {}", answer);
-        println!("==Real Thing==");
-        answer = solve(Path::new("./input"), *func, false);
-        println!("Total Fuel Requirements: {}", answer);
+    for (name, path, verbose) in [
+        ("Test", Path::new("./test"), true),
+        ("Real", Path::new("./input"), false),
+    ]
+    .iter()
+    {
+        println!("======={} Case=======", name);
+        for (idx, func) in [fuel_calc_v1, fuel_calc_v2].iter().enumerate() {
+            println!("==Part {}==", idx + 1);
+            println!("Total Fuel Requirements: {}", solve(path, *func, *verbose));
+        }
     }
 }
