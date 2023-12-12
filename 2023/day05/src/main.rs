d47255906eeb7124ee501c56bd54387002fe7980
Stephen Herbein <stephen@herbein.net>
2023-12-12 02:35:54 -0500

    solve day5 part 2

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,6 +1,8 @@
-use regex::Regex;
-use std::collections::{HashMap, HashSet};
+//use std::collections::{HashMap, HashSet};
 use std::env;
+
+use regex::Regex;
+use itertools::Itertools;
 
 fn read_file_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
@@ -13,7 +15,7 @@ fn read_file_lines(filename: &str) -> Vec<String> {
 }
 
 #[derive(Default,Debug)]
-struct Range {
+struct MapRange {
     destination_start: u64,
     source_start: u64,
     length: u64,
@@ -23,7 +25,7 @@ struct Range {
 struct Mapping {
     source: String,
     destination: String,
-    ranges: Vec<Range>,
+    ranges: Vec<MapRange>,
 }
 
 impl Mapping {
@@ -36,39 +38,110 @@ impl Mapping {
         }
         return val
     }
+
+    fn map_dest_src(&self, val: u64) -> u64 {
+        for range in self.ranges.iter() {
+            let dest_end = range.destination_start + range.length;
+            if val >= range.destination_start && val < dest_end {
+                return range.source_start + (val - range.destination_start)
+            }
+        }
+        return val
+    }
 }
 
-fn part1(lines: Vec<String>) {
-    let mut lines_iter = lines.iter();
-    let seeds = lines_iter
-        .next()
-        .unwrap()
-        .split_once(":")
-        .unwrap()
-        .1
-        .split(" ")
-        .filter_map(|x| x.parse::<u64>().ok())
-        .collect::<Vec<u64>>()
-        ;
-    println!("Seeds: {:#?}", seeds);
+#[derive(Default,Debug)]
+struct Range {
+    start: u64,
+    length: u64,
+}
 
+#[derive(Default,Debug)]
+struct Seeds {
+    seeds: Vec<Range>,
+}
+
+impl IntoIterator for Seeds {
+    type Item = u64;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.seeds.iter().flat_map(|seed| {
+            seed.start..seed.start+seed.length
+        }).collect::<Vec<u64>>().into_iter()
+    }
+}
+
+impl Seeds {
+    fn for_part1(line: String) -> Seeds {
+        let vals = line
+            .split_once(":")
+            .unwrap()
+            .1
+            .split(" ")
+            .filter_map(|x| x.parse::<u64>().ok())
+            .collect::<Vec<u64>>();
+        let seeds = vals.iter().map(|x| {
+            Range{
+                start: *x,
+                length: 1,
+            }
+        }).collect();
+        Seeds{
+            seeds
+        }
+    }
+
+    fn for_part2(line: String) -> Seeds {
+        let vals = line
+            .split_once(":")
+            .unwrap()
+            .1
+            .split(" ")
+            .filter_map(|x| x.parse::<u64>().ok())
+            .collect::<Vec<u64>>();
+        let seeds = vals.iter().chunks(2).into_iter().map(
+            |chunk| {
+                let mut it = chunk.into_iter();
+                Range{
+                    start: it.next().unwrap().clone(),
+                    length: it.next().unwrap().clone(),
+                }
+        }).collect();
+        Seeds{
+            seeds
+        }
+    }
+
+    fn is_valid(&self, possible_seed: u64) -> bool {
+        for seed in self.seeds.iter() {
+            if possible_seed >= seed.start && possible_seed < seed.start + seed.length {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+fn maps_from_lines<I>(lines_iter: I) -> Vec<Mapping>
+where
+    I: Iterator<Item = String>,
+{
     let mut maps = Vec::<Mapping>::new();
     let map_re = Regex::new(r"^([a-z]+)-to-([a-z]+) map:$").unwrap();
     for line in lines_iter.filter(|x| x.trim().len() > 0) {
-        if let Some(caps) = map_re.captures(line) {
+        if let Some(caps) = map_re.captures(line.as_str()) {
             let source = caps.get(1).unwrap().as_str().to_string();
             let destination = caps.get(2).unwrap().as_str().to_string();
-            println!("Map line: {} to {}", source, destination);
             maps.push(Mapping {
                 source,
                 destination,
                 ranges: Vec::new(),
             })
         } else {
-            println!("Range line: {}", line);
             let mut vals = line.split(" ");
             if let Some(map) = maps.last_mut() {
-                map.ranges.push(Range {
+                map.ranges.push(MapRange {
                     destination_start: vals.next().unwrap().parse().unwrap(),
                     source_start: vals.next().unwrap().parse().unwrap(),
                     length: vals.next().unwrap().parse().unwrap(),
@@ -78,22 +151,44 @@ fn part1(lines: Vec<String>) {
             }
         }
     }
-    println!("Maps: {:#?}", maps);
+    return maps;
+}
+
+fn part1(lines: Vec<String>) {
+    let mut lines_iter = lines.into_iter();
+    let seeds = Seeds::for_part1(lines_iter.next().unwrap());
+    let maps = maps_from_lines(lines_iter);
+
     let mut lowest_location = u64::MAX;
-    for seed in seeds.iter() {
+    for seed in seeds.into_iter() {
         let mut curr_value: u64 = seed.clone();
         for map in maps.iter() {
-            let prev_value = curr_value.clone();
             curr_value = map.map_src_dest(curr_value);
-            println!("{} {} mapped to {} {}", map.source, prev_value, map.destination, curr_value);
         }
-        println!("Seed {} finally mapped to {}\n", seed, curr_value);
         lowest_location = u64::min(lowest_location, curr_value);
     }
     println!("Part1: {}", lowest_location);
 }
 
-fn part2(lines: Vec<String>) {}
+fn part2(lines: Vec<String>) {
+    let mut lines_iter = lines.into_iter();
+    let seeds = Seeds::for_part2(lines_iter.next().unwrap());
+    let maps = maps_from_lines(lines_iter);
+
+    let mut lowest_location = 0;
+    for location in 0..u64::MAX {
+        let mut curr_value = location;
+        for map in maps.iter().rev() {
+            curr_value = map.map_dest_src(curr_value);
+        }
+        if seeds.is_valid(curr_value) {
+            lowest_location = location;
+            break;
+        }
+    }
+
+    println!("Part2: {}", lowest_location);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
