527f880624e711f42ee5cd453e0ac6dd8660c978
Stephen Herbein <stephen272@gmail.com>
2024-12-02 09:26:09 -0500

    gotta refactor in case we want to make the reactor more "safe" later

--- 2024/day02/solve.py
+++ 2024/day02/solve.py
@@ -16,14 +16,18 @@ def get_matrix(input_file):
 
     return matrix
 
-def row_is_safe(row):
+def get_all_conditions(row):
     diffs = [x-y for (x, y) in zip(row, row[1:])]
 
-    all_increasing = all([x > 0 for x in diffs])
-    all_decreasing = all([x < 0 for x in diffs])
-    within_threshold = all([abs(x) > 0 and abs(x) <= 3 for x in diffs])
+    increasing = [x > 0 for x in diffs]
+    decreasing = [x < 0 for x in diffs]
+    within_threshold = [abs(x) > 0 and abs(x) <= 3 for x in diffs]
 
-    return (all_increasing or all_decreasing) and within_threshold
+    return (increasing, decreasing, within_threshold)
+
+def row_is_safe(row):
+    increasing, decreasing, within_threshold =  get_all_conditions(row)
+    return (all(increasing) or all(decreasing)) and all(within_threshold)
 
 def row_is_safe_without_index(row, idx):
     return row_is_safe(row[0:idx] + row[idx+1:])
@@ -32,23 +36,11 @@ def indices_with_false(row):
     return [i for i, x in enumerate(row) if not x]
 
 def row_is_safe_with_dampening(row):
-    diffs = [x-y for (x, y) in zip(row, row[1:])]
-
-    all_increasing = [x > 0 for x in diffs]
-    indices = indices_with_false(all_increasing)
-    if len(indices) == 1:
-        return row_is_safe_without_index(row, indices[0]) or row_is_safe_without_index(row, indices[0]+1)
-
-    all_decreasing = [x < 0 for x in diffs]
-    indices = indices_with_false(all_decreasing)
-    if len(indices) == 1:
-        return row_is_safe_without_index(row, indices[0]) or row_is_safe_without_index(row, indices[0]+1)
-
-    within_threshold = [abs(x) > 0 and abs(x) <= 3 for x in diffs]
-    indices = indices_with_false(within_threshold)
-    if len(indices) == 1:
-        return row_is_safe_without_index(row, indices[0]) or row_is_safe_without_index(row, indices[0]+1)
-
+    conditions = get_all_conditions(row)
+    for condition in conditions:
+        indices = indices_with_false(condition)
+        if len(indices) == 1:
+            return row_is_safe_without_index(row, indices[0]) or row_is_safe_without_index(row, indices[0]+1)
     return False
 
 def part1(input_file):
