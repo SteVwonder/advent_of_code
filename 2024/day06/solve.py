7d45e788ad92cce2c40b06fcd3a5ac12d5a33152
Stephen Herbein <stephen272@gmail.com>
2024-12-06 01:08:08 -0500

    this really should work, but i need some more time. brute force for now!

--- 2024/day06/solve.py
+++ 2024/day06/solve.py
@@ -8,6 +8,27 @@ def get_lines(input_file):
         for line in infile:
             yield line
 
+def ninety_degrees(direction):
+    if direction == (-1, 0):
+        return(0, 1)
+    elif direction == (0, 1):
+        return(1, 0)
+    elif direction == (1, 0):
+        return(0, -1)
+    elif direction == (0, -1):
+        return(-1, 0)
+    else:
+        print(direction)
+        raise RuntimeError()
+
+def get_intersection_idx(direction, pos):
+    if direction in [(0,1), (0, -1)]:
+        return pos[0]
+    elif direction in [(-1, 0), (1, 0)]:
+        return pos[1]
+    else:
+        raise RuntimeError()
+
 class Layout:
     def __init__(self, input_file):
         lines = get_lines(input_file)
@@ -28,21 +49,17 @@ class Layout:
         # TODO: do loop variables exist after the for loop ends?
         self.dimensions = (num_lines, num_cols)
         self.cells_visited = {self.guard_pos}
+        self.virtual_walls = set()
+        self.new_blockades = set()
 
     def rotate_guard(self):
-        if self.guard_direction == (-1, 0):
-            self.guard_direction = (0, 1)
-        elif self.guard_direction == (0, 1):
-            self.guard_direction = (1, 0)
-        elif self.guard_direction == (1, 0):
-            self.guard_direction = (0, -1)
-        elif self.guard_direction == (0, -1):
-            self.guard_direction = (-1, 0)
-        else:
-            print(self.guard_direction)
-            raise RuntimeError()
+        self.guard_direction = ninety_degrees(self.guard_direction)
 
     def move_guard(self) -> bool:
+        self.virtual_walls.add(
+            (self.guard_direction, get_intersection_idx(self.guard_direction, self.guard_pos))
+        )
+
         while True:
             new_guard_pos = (self.guard_pos[0] + self.guard_direction[0],
                              self.guard_pos[1] + self.guard_direction[1])
@@ -53,6 +70,12 @@ class Layout:
                 return True
             elif new_guard_pos in self.obstacles:
                 return False
+
+            rotated_direction = ninety_degrees(self.guard_direction)
+            cycle_wall = (rotated_direction, get_intersection_idx(rotated_direction, self.guard_pos))
+            if cycle_wall in self.virtual_walls:
+                self.new_blockades.add(new_guard_pos)
+
             self.guard_pos = new_guard_pos
             self.cells_visited.add(new_guard_pos)
 
@@ -63,7 +86,10 @@ def part1(input_file):
     return len(layout.cells_visited)
 
 def part2(input_file):
-    pass
+    layout = Layout(input_file)
+    while not layout.move_guard():
+        layout.rotate_guard()
+    return len(layout.new_blockades)
 
 def main():
     parser = argparse.ArgumentParser()
