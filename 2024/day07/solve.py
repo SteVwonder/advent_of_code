5a0c2e3f83ac8f461addc5bc2b667c87625bd165
Stephen Herbein <stephen@herbein.net>
2024-12-07 10:13:03 -0500

    that was it??

--- 2024/day07/solve.py
+++ 2024/day07/solve.py
@@ -1,12 +1,21 @@
 import re
 import argparse
 import itertools
-from tqdm import trange
 
 def get_lines(input_file):
     with open(input_file, 'r') as infile:
         for line in infile:
             yield line
+
+def get_input(input_file) -> list[tuple[int, list[int]]]:
+    input = []
+    lines = get_lines(input_file)
+    for line in lines:
+        test_value, component_strs = line.split(": ")
+        test_value = int(test_value)
+        components = [int(x) for x in component_strs.split(" ")]
+        input.append((test_value, components))
+    return input
 
 def eval_ops(components, operators) -> int:
     ret = components[0]
@@ -14,25 +23,25 @@ def eval_ops(components, operators) -> int:
         ret = op(ret, x)
     return ret
 
-def attempt_solve(test_value, components) -> bool:
-    for operators in itertools.product([lambda x,y: x+y, lambda x,y: x*y], repeat=(len(components)-1)):
+def attempt_solve(test_value, components, functions) -> bool:
+    for operators in itertools.product(functions, repeat=(len(components)-1)):
         if test_value == eval_ops(components, operators):
             return True
     return False
 
 def part1(input_file):
-    lines = get_lines(input_file)
     answer = 0
-    for line in lines:
-        test_value, component_strs = line.split(": ")
-        test_value = int(test_value)
-        components = [int(x) for x in component_strs.split(" ")]
-        if attempt_solve(test_value, components):
+    for (test_value, components) in get_input(input_file):
+        if attempt_solve(test_value, components, [lambda x,y: x+y, lambda x,y: x*y]):
             answer += test_value
     return answer
 
 def part2(input_file):
-    pass
+    answer = 0
+    for (test_value, components) in get_input(input_file):
+        if attempt_solve(test_value, components, [lambda x,y: x+y, lambda x,y: x*y, lambda x,y: int(str(x) + str(y))]):
+            answer += test_value
+    return answer
 
 def main():
     parser = argparse.ArgumentParser()
